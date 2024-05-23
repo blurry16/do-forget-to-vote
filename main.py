db9e773697aff19501d575b7cf5971de5dd01930
blurry16 <147284253+blurry16@users.noreply.github.com>
2024-05-24 00:50:49 +0300

    Protection from stupidity added.
    Now it will raise an exception if MCNAME wasn't changed. (You won't be put in cooldown by planet mc if you forget to change it to your nickname)

--- main.py
+++ main.py
@@ -4,7 +4,9 @@ from time import time
 HEADERS = {
     "User-Agent": "Mozilla/5.0 (X11; Linux x86_64; rv:123.0) Gecko/20100101 Firefox/123.0"
 }
-MCNAME = "Your Minecraft nickname goes here."
+MCNAME = ""
+if MCNAME == "":
+    raise Exception("MCNAME not set")
 
 start_time = time()
 
@@ -14,6 +16,10 @@ try:
         headers=HEADERS,
         data={"mcname": MCNAME, "servervote": "1"},
     )
-    print(f"Finished with no exceptions for {round(time() - start_time, 1)} seconds. Enjoy ^w^!")
+    print(
+        f"Finished with no exceptions for {round(time() - start_time, 1)} seconds. Enjoy ^w^!"
+    )
 except Exception as e:
-    print(f"Exception {e} occurred. Try again later. ({round(time() - start_time, 1)} seconds elapsed)")
+    print(
+        f"Exception {e} occurred. Try again later. ({round(time() - start_time, 1)} seconds elapsed)"
+    )
